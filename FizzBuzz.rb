f0b415135e3e241805e41dc6990f4458f51307b2
John Thornton <John.H.Thornton@jpmchase.com>
2016-01-14 13:17:47 -0500

    Last Commit

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,5 +1,5 @@
 #Iteration to 100
-
+=begin
 puts "Hello, I am printing to 100"
 i = 0
 while i <= 100
@@ -16,3 +16,36 @@ while i <= 100
 		end
 	i += 1
 end
+=end
+
+
+puts "Hello, I am printing to 100"
+[0...100].each do |i|
+puts case 
+		when i % 3 == 0 && i % 5 == 0
+			puts 'FizzBuzz'
+		when i % 3 == 0
+			puts "Fizz"
+		when i % 5 == 0
+			puts 'Buzz'
+		else 
+			puts i
+		end
+	i += 1
+end
+
+=begin
+puts "Hello, I am printing to 100"
+var=l[0...100]
+var.each do |x|
+	if x % 3 == 0 && x % 5 == 0
+		puts "FizzBuzz"
+
+	elsif x % 3 == 0
+		puts "Fizz"
+
+	elsif x % 5 == 0
+		puts "Buzz"
+	end		
+end
+=end
