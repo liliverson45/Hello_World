54d7f3b92a92a672b265686bacbeb276f3163007
john <liliverson45@yahoo.com>
2017-10-18 16:33:25 -0400

    Adding methods

--- hello_world2_v2.rb
+++ hello_world2_v2.rb
@@ -5,50 +5,59 @@
 #Adding a counter to allow you to repeat this program as much as you want. 
 
 #======================================
-print "Hello, please enter your name: "
-a = gets.chomp.strip
-while a == "" 
-	puts "I'm sorry, this field cannot be blank."
+def get_UserName(a)
+
 	print "Hello, please enter your name: "
-	a = gets.chomp.strip	
+	a = gets.chomp.strip
+	while a == "" 
+		puts "I'm sorry, this field cannot be blank."
+		print "Hello, please enter your name: "
+		a = gets.chomp.strip	
+	end
 end 
 
-f_name = []
-puts "#{a}, please enter your 5 favorite names: "
-5.times do |i|
-	print "Favorite Name #{i}: "
-	name = gets.chomp.strip
-	while name == ""
-		puts "I'm sorry, this field cannot be blank."
-		puts "#{a}, please enter your 5 favorite names: "
+def get_FavName(f_name)
+
+	f_name = []
+	puts "#{a}, please enter your 5 favorite names: "
+	5.times do |i|
 		print "Favorite Name #{i}: "
 		name = gets.chomp.strip
-	end	
-f_name << name
-
-end
-
-f_color = []
-puts "#{a}, please enter your 5 favorite colors: "
-5.times do |c|
-	print "Favorite Color #{c}: "
-	color = gets.chomp.strip
-	while color == ""
-		puts "I'm sorry, this field cannot be blank."
-		puts "#{a}, please enter your 5 favorite colors: "
-		print "Favorite Color #{c}: "
-		color = gets.chomp.strip		
+		while name == ""
+			puts "I'm sorry, this field cannot be blank."
+			puts "#{a}, please enter your 5 favorite names: "
+			print "Favorite Name #{i}: "
+			name = gets.chomp.strip
+		end	
+	f_name << name
 	end
-f_color << color
 end
 
-hashes = []
-f_name.each_with_index do |v, i|
-	hashes << {name: f_name[i], color: f_color[i]}
+def get_FavColor(f_color)
+	f_color = []
+	puts "#{a}, please enter your 5 favorite colors: "
+	5.times do |c|
+		print "Favorite Color #{c}: "
+		color = gets.chomp.strip
+		while color == ""
+			puts "I'm sorry, this field cannot be blank."
+			puts "#{a}, please enter your 5 favorite colors: "
+			print "Favorite Color #{c}: "
+			color = gets.chomp.strip		
+		end
+	f_color << color
+	end
 end
 
-hashes.each do |h|
-	puts "#{a}, #{h[:color]} is the favorite color of #{h[:name]}."
+def Display_Name(hashes)
+	hashes = []
+	f_name.each_with_index do |v, i|
+		hashes << {name: f_name[i], color: f_color[i]}
+	end
+
+	hashes.each do |h|
+		puts "#{a}, #{h[:color]} is the favorite color of #{h[:name]}."
+	end
 end
 
 #=======================================
@@ -111,12 +120,10 @@ answer = gets.chomp.strip.downcase
 		hashes.each do |h|
 			puts "#{a}, #{h[:color]} is the favorite color of #{h[:name]}."
 		end
+	initialize answer	
+
 	end
-# Once the user goes through this loop it doesn't ask at question again.
-# Loop not working. 
-	else
-		puts "Good Bye"
-	end
+ 		puts "Good Bye"
 			
 
 
